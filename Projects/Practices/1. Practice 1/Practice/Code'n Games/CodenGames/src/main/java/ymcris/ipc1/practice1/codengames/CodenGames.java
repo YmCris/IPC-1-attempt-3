f88f308e8ec5c1afc500348b1ce815fa6d369fcd
Cris <ymcrisystems@gmail.com>
2025-02-04 20:33:34 -0600

    Creando practica 1

---
+++ Projects/Practices/1. Practice 1/Practice/Code'n Games/CodenGames/src/main/java/ymcris/ipc1/practice1/codengames/CodenGames.java
@@ -0,0 +1,12 @@
+package ymcris.ipc1.practice1.codengames;
+
+/**
+ *
+ * @author YmCris
+ */
+public class CodenGames {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
